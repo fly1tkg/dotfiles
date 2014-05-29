8e346e8662476e8c6800918c0510741c9ef8dffa
Shoichi Takagi <fly1tkg@gmail.com>
2014-05-29 12:07:49 +0900

    gitのプロトコルで取得するように変更

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ rm -f ~/.vimrc
 rm -f ~/.gvimrc
 rm -f ~/.zshrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-git clone https://github.com/fly1tkg/dotfiles.git ~/dotfiles
+git clone git@github.com:fly1tkg/dotfiles.git ~/dotfiles
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gvimrc ~/.gvimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
